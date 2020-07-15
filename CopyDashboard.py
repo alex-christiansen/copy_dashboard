dbc3cee785a5bc9e255d6d32124a28952b67d2c5
alex-christiansen <52046910+alex-christiansen@users.noreply.github.com>
2020-07-15 08:56:13 -0600

    Update CopyDashboard.py

--- CopyDashboard.py
+++ CopyDashboard.py
@@ -3,25 +3,33 @@ import logging
 import urllib3
 import requests
 from looker_sdk import models
+
 # disable warnings coming from self-signed SSL cert
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 sdk = looker_sdk.init31() 
-###### Grab Info about New Dashboard #####
-NewTitle = input("Enter title for new dashboard: ")
-BaseDashboard = input("ID of dashboard to copy: ")
-############## Step 1 - Get all information about the dashboard to copy ###################
+
+############## Grab information about dashboard you'd like to copy  ##############
+# NewTitle = input("Enter title for new dashboard: ")
+# BaseDashboard = input("ID of dashboard to copy: ")
+
+NewTitle = 'Dashboard Copy' # Enter a title for the new copied dashboard
+BaseDashboard = '000' # Enter the dashboard id of the dashboard you'd like to copy
+
+################ Step 1 - Get all information about the dashboard to copy ################
 def GetDashboard(dashboard_id=None):
     DashboardToCopy = sdk.dashboard(dashboard_id=str(dashboard_id))
     return DashboardToCopy
 DashboardToCopy = GetDashboard(BaseDashboard)
+
 # Print high level info about dashboard to copy
 print('Copying over the ' + DashboardToCopy.title + ' dashboard\n',
       'It is located in the ' + DashboardToCopy.folder.name + ' folder\n',
       'It has ' + str(len(DashboardToCopy.dashboard_filters)) + ' filter(s), '
       + str(len(DashboardToCopy.dashboard_elements)) + ' elements(s) and '
       + str(len(DashboardToCopy.dashboard_layouts)) + ' layout(s).')
-############################################################################################
-############## Step 2 - Create blank dashboard with same meta data as base dashboard #######
+###########################################################################################
+
+############# Step 2 - Create blank dashboard with same meta data as base dashboard #######
 def CreateDashboard(
                     description= None
                     ,hidden = None
@@ -70,12 +78,13 @@ def CreateDashboard(
     )
     CreatedDashboard = sdk.create_dashboard(body=DashboardObject)
     return CreatedDashboard
+
 NewDashboardBase = CreateDashboard(
         description= DashboardToCopy.description
         ,hidden = DashboardToCopy.hidden
         ,query_timezone= DashboardToCopy.query_timezone
         ,refresh_interval= DashboardToCopy.refresh_interval
-        ,title= NewTitle + " Copy"
+        ,title= NewTitle + " Copy" # Add in 'Copy' to dashboard title just in case that title has already been taken
         ,background_color=DashboardToCopy.background_color
         ,crossfilter_enabled= DashboardToCopy.crossfilter_enabled
         ,deleted=DashboardToCopy.deleted
@@ -91,33 +100,16 @@ NewDashboardBase = CreateDashboard(
         ,appearance= DashboardToCopy.appearance
         ,preferred_viewer= DashboardToCopy.preferred_viewer
     )
-# Print out metadata of new and old dashboard
-print('Description: ' + str(DashboardToCopy.description))
-print('Hidden: ' + str(DashboardToCopy.hidden))
-print('Query Timezone: ' + str(DashboardToCopy.query_timezone))
-print('Refresh Interval: ' + str(DashboardToCopy.refresh_interval))
-print('Title: ' + str(DashboardToCopy.title))
-print('Background Color: ' + str(DashboardToCopy.background_color))
-print('Crossfilter Enabled: ' + str(DashboardToCopy.crossfilter_enabled))
-print('Deleted: ' + str(DashboardToCopy.deleted))
-print('Load Config: ' + str(DashboardToCopy.load_configuration))
-print('LookML Link ID: ' + str(DashboardToCopy.lookml_link_id))
-print('Show Filters Bar: ' + str(DashboardToCopy.show_filters_bar))
-print('Show Title: ' + str(DashboardToCopy.show_title))
-print('Folder ID: ' + str(DashboardToCopy.folder_id))
-print('Text Tile Text Color: ' + str(DashboardToCopy.text_tile_text_color))
-print('Text Tile Background Color: ' + str(DashboardToCopy.tile_background_color))
-print('Tile Text Color' + str(DashboardToCopy.tile_text_color))
-print('Title Color: ' + str(DashboardToCopy.title_color))
-print('Appearance: ' + str(DashboardToCopy.appearance))
-print('Preferred Viewer: ' + str(DashboardToCopy.preferred_viewer))
+
 ############################################################################################
-############## Step 3 - Get all filters from base dashboard and add to newly created dashboard #######
+
+##### Step 3 - Get all filters from base dashboard and add to newly created dashboard #######
 # Grab filters from base dashboard
 AllFilters = sdk.dashboard_dashboard_filters(dashboard_id=str(DashboardToCopy.id))
+
 # Add to new dashboard
 for f in AllFilters:
-    print('Filter Name: ' + f.name + ', Row: ' + str(f.row))
+    # print('Filter Name: ' + f.name + ', Row: ' + str(f.row)) # Print out filter names and filter row location if you'd like
     DashboardFilterObject = looker_sdk.models.WriteCreateDashboardFilter(
                                                     dashboard_id=NewDashboardBase.id,
                                                     name=f.name,
@@ -133,13 +125,14 @@ for f in AllFilters:
                                                     required=f.required
                                                     )
     NewFilter = sdk.create_dashboard_filter(body=DashboardFilterObject)
-############################################################################################
-############## Step 4 - Get all elements from base dashboard and add to newly created dashboard #######
+##############################################################################################
+
+###### Step 4 - Get all elements from base dashboard and add to newly created dashboard #######
 DashboardElementsToCopy = sdk.dashboard_dashboard_elements(dashboard_id=str(DashboardToCopy.id))
+
+# Add each element from base dashboard to new copied dashboard
 for f in DashboardElementsToCopy:
-    print('Element Name: ' + f.title + ' on Dashboard: ' + str(NewDashboardBase.id))
-    # print('Element Name: ' + str(f.result_maker))
-    print('Element Name: ' + str(f.query_id))
+    # print('Element Name: ' + f.title + ' on Dashboard: ' + str(NewDashboardBase.id)) # print title of tile
     DashboardElementObject = looker_sdk.models.WriteDashboardElement(
         body_text= f.body_text,
         dashboard_id= NewDashboardBase.id,
@@ -159,8 +152,10 @@ for f in DashboardElementsToCopy:
         type= f.type
     )
     CreatedDashboardElement = sdk.create_dashboard_element(body = DashboardElementObject)
+
 ############################################################################################
-############## Step 5 - Create all dashboard elements #######
+
+########################## Step 5 - Create all dashboard elements ##########################
 DashboardLayoutsToCopy = sdk.dashboard_dashboard_layouts(dashboard_id=str(DashboardToCopy.id))
 for f in DashboardLayoutsToCopy:
     print('Layout ID: ' + f.id + ' Layout Type: ' + f.type)
@@ -173,10 +168,13 @@ for f in DashboardLayoutsToCopy:
     )
     CreatedDashboardLayout = sdk.create_dashboard_layout(body = DashboardElementObject)
 ############################################################################################
-############## Step 6 - Update Layouts #####################################################
-## Get layouts of new dashboard
+
+############################### Step 6 - Update Layouts ####################################
+
+# Get layouts and elements of new dashboard
 NewDashboardLayouts = sdk.dashboard_dashboard_layouts(NewDashboardBase.id) 
 NewDashboardElements = sdk.dashboard_dashboard_elements(NewDashboardBase.id)
+
 def UpdateDashboardLayoutComponent(
                             dashboard_layout_component_id=None,
                             dashboard_layout_id= None,
@@ -196,7 +194,7 @@ def UpdateDashboardLayoutComponent(
                         )
     UpdatedDashboardLayoutComponent = sdk.update_dashboard_layout_component(dashboard_layout_component_id=str(dashboard_layout_component_id), body = LayoutComponentObject)
     return UpdatedDashboardLayoutComponent
-############################################################################################
+
 for i in NewDashboardLayouts[0].dashboard_layout_components:
     for j in DashboardLayoutsToCopy[0].dashboard_layout_components:
         if i.element_title == j.element_title:
@@ -210,7 +208,5 @@ for i in NewDashboardLayouts[0].dashboard_layout_components:
                 width= j.width,
                 height= j.height
                 )
-            print('Old Title: ' + j.element_title)
-            print('Old Row: ' + str(j.row))
-            print('New Title: ' + i.element_title)
-            print('New Row: ' + str(j.row))
+
+############################################################################################
